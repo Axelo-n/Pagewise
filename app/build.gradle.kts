27e575a1923f9abd65f7072e4793fc0a0fc6205c
Axelo-n <mkev46p@gmail.com>
2026-04-04 09:17:51 +0700

    Fix: Forgot to change app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.pagewise"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "3.1"
+        versionCode = 8
+        versionName = "3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
